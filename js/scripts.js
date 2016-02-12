d7d74c7184d8ccb99fcdbe73844a33332bcaf07d
Kassidy Douglas <kcmdouglas@gmail.com>
2016-02-12 08:04:35 -0800

    Pass Pizza constructor spec, create Topping class and failing Topping constructor spec.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ function Pizza(pizzaSize) {
   this.pizzaSize = pizzaSize;
   this.toppings = [];
 }
+
+function Topping(name) {
+  this.name = name;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,13 @@
 describe('Pizza', function () {
   it("creates a pizza object with a given size", function (){
     var newPizza = new Pizza(1);
-    expect(newPizza.pizzaSize).to.equal(0);
-  })
-})
+    expect(newPizza.pizzaSize).to.equal(1);
+  });
+});
+
+describe('Topping', function () {
+  it("creates a topping object with a name", function (){
+    var newTopping = new Topping("Anchovies");
+    expect(newTopping.name).to.equal("Mushrooms");
+  });
+});
