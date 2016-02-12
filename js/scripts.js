de0be2fa6eecb861a47e9d9f654a379ac6707a65
Kassidy Douglas <kcmdouglas@gmail.com>
2016-02-12 09:15:34 -0800

    Create failing spec to ensure Pizza object holds array of toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,26 @@ function Pizza(pizzaSize) {
 function Topping(name) {
   this.name = name;
 }
+
+function Order(address) {
+  this.address = address;
+  this.pizzas = [];
+}
+
+$(document).ready(function() {
+  $("form#add-pizza").submit(function (event){
+    event.preventDefault();
+
+    var inputtedSize = parseInt($("select#size").val());
+    var newPizza = new Pizza(inputtedSize);
+    $("input[name='toppings[]']:checked").each(function () {
+      var toppingName = $(this).val();
+      newPizza.toppings.push(toppingName);
+    });
+
+
+
+    $("div#entire-order").append("<li><span class='ordered-pizza'>" + newContact.fullName() + "</span></li>");
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -3,11 +3,25 @@ describe('Pizza', function () {
     var newPizza = new Pizza(1);
     expect(newPizza.pizzaSize).to.equal(1);
   });
+  it("holds an array of toppings for each pizza", function (){
+    var newPizza = new Pizza(1);
+    var firstTopping = new Topping("Peppers");
+    var secondTopping = new Topping("Onions");
+    newPizza.toppings.push(firstTopping);
+    newPizza.toppings.push(secondTopping);
+    expect(newPizza.toppings[0].name).to.equal("Onions");
+    expect(newPizza.toppings[1].name).to.equal("Pepperoni");
+  });
 });
 
 describe('Topping', function () {
   it("creates a topping object with a name", function (){
     var newTopping = new Topping("Anchovies");
     expect(newTopping.name).to.equal("Anchovies");
+  });
+});
+
+describe('Order', function () {
+  it("", function (){
   });
 });
